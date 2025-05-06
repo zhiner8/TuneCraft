6c7c059d0369d48872732d0a204215a6eb757491
onitiri <onitiri42@gmail.com>
2025-05-06 18:42:50 -0400

    Render Home Page

---
+++ craft.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "templates"))
+
+const PORT = process.env.PORT || 10000;  // Default to 10000 if PORT is not set
+
+app.get('/', (req, res) => {
+  res.render("craft")
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is listening on http://0.0.0.0:${PORT}`);
+});
