d44cbcade606d269db7c32154a2f5c38401a7153
Dominick Cardone <cardoned11@gmail.com>
2025-05-07 12:43:23 -0400

    initial form structure, changed image and style paths

--- craft.js
+++ craft.js
@@ -3,12 +3,19 @@ const app = express();
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "templates"))
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 10000;  
 
 
 app.get('/', (req, res) => {
-  res.render("craft")
+  res.render("craft");
+});
+
+app.get("/selectMood", (req, res) => {
+  res.render("selectMood");
 });
 
 app.listen(PORT, '0.0.0.0', () => {
